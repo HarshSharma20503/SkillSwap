25ade03dcc131153e92f53c7629547a2813050c7
Harsh Sharma <harshsharma20503@gmail.com>
2024-02-22 02:44:33 +0530

    login page in progress

--- Frontend/src/Pages/Login/Login.jsx
+++ Frontend/src/Pages/Login/Login.jsx
@@ -1,5 +1,17 @@
 const Login = () => {
-  return <div>Login</div>;
+  // const [message, setMessage] = useState("");
+
+  const handleGoogleLogin = () => {
+    window.location.href = "http://localhost:5000/auth/google";
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <button onClick={handleGoogleLogin}>Login with Google</button>
+      {/* <p>{message}</p> */}
+    </div>
+  );
 };
 
 export default Login;
