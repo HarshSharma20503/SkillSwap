3b52dcc49ebf5889f4f74417c849cfae6d22d500
Harsh Sharma <harshsharma20503@gmail.com>
2024-02-21 08:15:44 +0530

    added login controller blueprint

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -4,4 +4,8 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "User registered successfully" });
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "User Login successfully" });
+});
+
 export { registerUser };
