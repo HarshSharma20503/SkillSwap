7048ea121d9e7121ef31f262116cd297943501c1
Harsh Sharma <harshsharma20503@gmail.com>
2024-02-21 08:12:57 +0530

    setup of react router dom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Discover from "./Pages/Discover/Discover";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        {/* Routes go here */}
+        <Route path="/" element={<Discover />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
