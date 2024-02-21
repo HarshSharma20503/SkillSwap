b8f024afbd614c05c7ad8874ffcceb632bf0faf4
Harsh Sharma <harshsharma20503@gmail.com>
2024-02-22 02:07:19 +0530

    added footer and mui

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,5 +1,50 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "96vh",
+      }}
+    >
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light" ? theme.palette.grey[200] : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {"Copyright © "}
+            <Link color="inherit" href="/">
+              SkillSwap
+            </Link>
+            &nbsp;
+            {new Date().getFullYear()}
+            {"."}
+          </Typography>
+        </Container>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: "#f44336",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
