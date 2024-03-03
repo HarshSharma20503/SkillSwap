4ecd78a10eb3fb01bb495f445d733581a3288c49
paakhim10 <paakhim10@gmail.com>
2024-03-03 21:40:07 +0530

    Updated AboutUs.jsx

--- Frontend/src/Pages/AboutUs/AboutUs.jsx
+++ Frontend/src/Pages/AboutUs/AboutUs.jsx
@@ -44,10 +44,12 @@ const AboutUs = () => {
       <div style={contentContainerStyle}>
         <h2 style={titleStyle}>About Us</h2>
         <p style={descriptionStyle}>
-          At SkillSwap, we believe in the power of learning and sharing knowledge. Our platform connects individuals from diverse backgrounds to exchange practical skills and expertise. Whether you're a seasoned professional looking to mentor others or a beginner eager to learn, SkillSwap provides a supportive environment for growth and collaboration.
+          <i>As students, we have looked for upskilling everywhere. Mostly, we end up paying big amounts to gain certifications and learn relevant skills. We thought of SkillSwap to resolve that. Learning new skills and gaining more knowledge all while networking with talented people!</i>
         </p>
         <p style={descriptionStyle}>
-          Our mission is to empower individuals to unlock their full potential through skill sharing. By facilitating meaningful interactions and fostering a culture of lifelong learning, we aim to create a community where everyone has the opportunity to thrive.
+          <br/>
+        At SkillSwap, we believe in the power of learning and sharing knowledge. Our platform connects individuals from diverse backgrounds to exchange practical skills and expertise. Whether you're a seasoned professional looking to mentor others or a beginner eager to learn, SkillSwap provides a supportive environment for growth and collaboration.
+        <br/><br/>Our mission is to empower individuals to unlock their full potential through skill sharing. By facilitating meaningful interactions and fostering a culture of lifelong learning, we aim to create a community where everyone has the opportunity to thrive.
         </p>
       </div>
         <img src={'/assets/images/about us.png'} style={{maxWidth: '50vw'}}/>
