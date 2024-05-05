1b3ec56f521c0e2ce349192bf655691fd8af519d
paakhim10 <paakhim10@gmail.com>
2024-05-05 15:20:27 +0530

    Profile page made responsive

--- Frontend/src/Pages/Profile/Profile.jsx
+++ Frontend/src/Pages/Profile/Profile.jsx
@@ -33,27 +33,29 @@ const Profile = () => {
 
   return (
     <div className="profile-container">
-      <div className="profile-box">
-        {/* Profile Photo */}
-        <div className="profile-photo">
-          <img src="/assets/images/sample_profile.jpg" alt="Profile" />
-        </div>
-        {/* Name */}
-        <div>
-          <h1 className="profile-name" style={{ marginLeft: "2rem" }}>
-            Paakhi Maheshwari
-          </h1>
-          {/* Rating */}
-          <div className="rating" style={{ marginLeft: "2rem" }}>
-            {/* Rating stars */}
-            <span className="rating-stars">⭐⭐⭐⭐⭐</span>
-            {/* Rating out of 5 */}
-            <span className="rating-value">(5)</span>
+        <div className="profile-box">
+        <div className="left-div">
+          {/* Profile Photo */}
+          <div className="profile-photo">
+            <img src="/assets/images/sample_profile.jpg" alt="Profile" />
           </div>
-          {/* Connect and Report Buttons */}
-          <div className="buttons">
-            <button className="connect-button">Connect</button>
-            <button className="report-button">Report</button>
+          {/* Name */}
+          <div className="misc">
+            <h1 className="profile-name" style={{ marginLeft: "2rem" }}>
+              Paakhi Maheshwari
+            </h1>
+            {/* Rating */}
+            <div className="rating" style={{ marginLeft: "2rem" }}>
+              {/* Rating stars */}
+              <span className="rating-stars">⭐⭐⭐⭐⭐</span>
+              {/* Rating out of 5 */}
+              <span className="rating-value">(5)</span>
+            </div>
+            {/* Connect and Report Buttons */}
+            <div className="buttons">
+              <button className="connect-button">Connect</button>
+              <button className="report-button">Report</button>
+            </div>
           </div>
         </div>
         <div className="portfolio-links">
