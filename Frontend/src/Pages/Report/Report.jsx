dc4da126f69ade45e2ba2483652ec4e94a4f01bd
paakhim10 <paakhim10@gmail.com>
2024-05-05 14:19:17 +0530

    Report form drafted

---
+++ Frontend/src/Pages/Report/Report.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Report.css';
+
+const ReportForm = () => {
+  return (
+    <div className="form-container">
+      <h1>REPORT PROFILE</h1>
+      <form>
+        <div className="form-box">
+            <div className="form-group">
+            <label className="question" htmlFor="username">Your Username</label>
+            <input type="text" id="username" className="form-control" placeholder="Enter your username" />
+            </div>
+            <div className="form-group">
+            <label className="question" htmlFor="reportedUsername">Username to be reported</label>
+            <input type="text" id="reportedUsername" className="form-control" placeholder="Enter username to be reported" />
+            </div>
+            <div className="form-group">
+            <label className="question" >Were you trained by this person?</label>
+            <div className="radio-group">
+                <input type="radio" id="yes" name="trained" value="Yes" />
+                <label htmlFor="yes">Yes</label>
+                <input type="radio" id="no" name="trained" value="No" />
+                <label htmlFor="no">No, I was the trainer.</label>
+            </div>
+            </div>
+            <div className="form-group">
+            <label className="question">What was the nature of the issue?</label>
+            <div className="radio-group">
+                <input type="radio" id="conduct" name="issue" value="Personal conduct" />
+                <label htmlFor="conduct">Personal conduct</label>
+                <input type="radio" id="expertise" name="issue" value="Professional expertise" />
+                <label htmlFor="expertise">Professional expertise</label>
+                <input type="radio" id="others" name="issue" value="Others" />
+                <label htmlFor="others">Others</label>
+            </div>
+            </div>
+            <div className="form-group">
+            <label className="question" htmlFor="issueDescription">Describe the issue to us</label>
+            <textarea id="issueDescription" className="form-control textarea-control" placeholder="Enter description"></textarea>
+            </div>
+            <button type="submit" className="submit-button">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ReportForm;
